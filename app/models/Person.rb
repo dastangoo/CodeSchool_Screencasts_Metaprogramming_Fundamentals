76a9349620415ef22c38686314b8e461e2459cbb
dastangoo <hamed.dastangoo@gmail.com>
2018-06-05 12:33:46 +0430

    Metaprogramming Fundamentals - Ruby Object Model

---
+++ app/models/Person.rb
@@ -0,0 +1,9 @@
+class Person
+  def initialize(first_name, last_name)
+    @first_name = first_name
+    @last_name = last_name
+  end
+  def format_name
+    "#{@first_name} #{@last_name}"
+  end
+end
